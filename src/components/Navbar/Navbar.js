9d795881245cbda13b903185074cc2c3d3e7088d
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-02 01:17:07 +0300

    add navbar and style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,19 @@
+import './Navbar.css';
+
+import moon from '../../imgs/moon-regular.svg';
+
+
 function Navbar(){
   return (
-    <div>
-      Navbar
-    </div>
+    <>
+      <nav className="navbar">
+        <h1>Where in the world?</h1>
+        <div className="toggle">
+          <img src={moon} />
+          <h2>Dark Mode</h2>
+        </div>
+      </nav>
+    </>
   )
 }
 

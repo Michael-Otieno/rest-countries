d0d629c5580cfb2c093ee746e61aecbb8b8d9efd
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-07 16:07:17 +0300

    make website responsive

--- src/components/CountryDetails/CountryDetails.js
+++ src/components/CountryDetails/CountryDetails.js
@@ -1,81 +1,94 @@
-import {Link, useParams} from 'react-router-dom';
-import arrow from '../../imgs/arrow.png';
-import './CountryDetails.css';
-import {useEffect, useState} from 'react';
-import {apiUrl} from '../util/api';
+import { Link, useParams } from "react-router-dom";
+import arrow from "../../imgs/arrow.png";
+import "./CountryDetails.css";
+import { useEffect, useState } from "react";
+import { apiUrl } from "../util/api";
 
-
-function CountryDetails(){
+function CountryDetails() {
   const [country, setCountry] = useState([]);
   const [error, setError] = useState("");
 
-  const {name} = useParams();
+  const { name } = useParams();
   const borders = country.map((country, key) => country.borders);
   const languages = country.map((country) => country.languages);
 
-
   useEffect(() => {
     const getCountryByName = async () => {
-      try{
+      try {
         const res = await fetch(`${apiUrl}/name/${name}`);
         if (!res.ok) throw new Error("Not found");
-        const data = await res.json()
-        setCountry(data)
-
-      }catch (error){
+        const data = await res.json();
+        setCountry(data);
+      } catch (error) {
         setError(error.message);
       }
-    }
-    getCountryByName()
-  }, [name])
+    };
+    getCountryByName();
+  }, [name]);
 
   return (
-    <div className='country-detail-div'>
-      <Link className='link' to='/'>
-        <button className='back--btn'>
-          <img className='arrow-left' src={arrow} alt="left arrow" />
+    <div className="country-detail-div">
+      <Link className="link" to="/">
+        <button className="back--btn">
+          <img className="arrow-left" src={arrow} alt="left arrow" />
           <span>Back</span>
         </button>
       </Link>
       <div>
-      {error && { error }}
+        {error && { error }}
         {country?.map((country, index) => (
-          <div className='country' key={index}>
+          <div className="country" key={index}>
             <img src={country.flags.png} />
-            <h3>{country.name.common}</h3>
-            <p>Native Name: 
-              <span>{country.name.official}</span>
-            </p>
-            <p>Population: 
-              <span>{country.population}</span>
-            </p>
-            <p>Region: 
-              <span>{country.region}</span>
-            </p>
-            <p>Sub Region: 
-              <span>{country.subregion}</span>
-            </p>
-            <p>Capital: 
-              <span>{country.capital}</span>
-            </p>
-            <p className='domain'>Top Level Domain: 
-              <span>{country.tld}</span>
-            </p>
-            <p>Currencies: 
-              <span>{}</span>
-            </p>
-            <p>Languages: 
-              <span></span>
-            </p>
-            <p className='borders'>Border Countries: <br/>
-              <span>{country.borders}</span>
-            </p>
-          
+            <div>
+              <h3>{country.name.common}</h3>
+              <div className="name-level-flex">
+                <div className="name">
+                  <p>
+                    Native Name:
+                    <span>{country.name.official}</span>
+                  </p>
+                  <p>
+                    Population:
+                    <span>{new Intl.NumberFormat().format(country.population)}</span>
+                  </p>
+                  <p>
+                    Region:
+                    <span>{country.region}</span>
+                  </p>
+                  <p>
+                    Sub Region:
+                    <span>{country.subregion}</span>
+                  </p>
+                  <p>
+                    Capital:
+                    <span>{country.capital}</span>
+                  </p>
+                </div>
+                <div className="top-domain">
+                  <p className="domain">
+                    Top Level Domain:
+                    <span>{country.tld}</span>
+                  </p>
+                  <p>
+                    Currencies:
+                    <span>{}</span>
+                  </p>
+                  <p>
+                    Languages:
+                    <span></span>
+                  </p>
+                </div>
+              </div>
+              <p className="borders">
+                Border Countries: 
+                <span>{country.borders}</span>
+              </p>
+            </div>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default CountryDetails;
